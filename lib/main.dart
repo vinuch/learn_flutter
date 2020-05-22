da1b6e0c299aa1cd38c57850b1144e4fb5dca3ec
vinuch <vincentedeh42@gmail.com>
2020-05-22 15:21:19 -0700

    completed boss level challenge - magic 8 ball flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+      MaterialApp(home: BallPage()),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text("Ask Me Anything"),
+        centerTitle: true,
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int imageNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  imageNumber = Random().nextInt(4) + 1;
+                });
+              },
+              child: Image.asset('images/ball$imageNumber.png'),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
